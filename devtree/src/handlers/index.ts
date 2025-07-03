b03f6c6b897e70862f40567a7458b53c7e679046
Aslly Zuñiga <asllyz2610@gmail.com>
2025-07-02 19:33:09 -0500

    Add user handle setup and input validation with Express Validator

--- devtree/src/handlers/index.ts
+++ devtree/src/handlers/index.ts
@@ -1,19 +1,37 @@
 import type { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const userExists = await User.findOne({ email });
+  //Manejar errores
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  return;
 
+  const { email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
   if (userExists) {
     const error = new Error("El usuario ya esta registrado");
     return res.status(409).json({ error: error.message });
   }
 
+  const handle = slug(req.body.handle, "");
+  const handleExists = await User.findOne({ handle });
+  if (handleExists) {
+    const error = new Error("Nombre de usuario no disponible");
+    return res.status(409).json({ error: error.message });
+  }
+
   const user = new User(req.body);
   user.password = await hashPassword(password);
-  await user.save();
+  user.handle = handle;
+
+  console.log(slug(handle, ""));
 
   await user.save();
   res.status(201).send("Registro Creado Correctamente");
--- devtree/src/models/User.ts
+++ devtree/src/models/User.ts
@@ -1,28 +1,36 @@
 import mongoose, { Schema } from "mongoose";
 
-interface IUser{
-  name: String
-  email: String
-  password: String
+interface IUser {
+  handle: string
+  name: String;
+  email: String;
+  password: String;
 }
 
 const userSchema = new Schema({
-    name:{
-        type: String,
-        require: true,
-        trim: true
-    },
-      email:{
-        type: String,
-        require: true,
-        trim: true,
-        unique: true
-    },
-      password:{
-        type: String,
-        require: true,
-        trim: true
-    }
-}) 
-const User = mongoose.model<IUser>('User', userSchema)
-export default User
+  handle: {
+    type: String,
+    require: true,
+    trim: true,
+    lowercase: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+});
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
