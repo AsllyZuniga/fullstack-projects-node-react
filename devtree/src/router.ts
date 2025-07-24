5f8c389fc4aa22ea3770cb133b5d4526a02d989a
Aslly Zuñiga <asllyz2610@gmail.com>
2025-07-24 18:46:37 -0500

    integrate input validation middleware into router for cleaner handlers

--- devtree/src/router.ts
+++ devtree/src/router.ts
@@ -2,26 +2,30 @@ import { Router } from "express";
 import { body } from "express-validator";
 
 import { createAccount, login } from "./handlers";
+import { handleInputErrors } from "./middleware/validation";
 
 const router = Router();
 
 //Autenticación y Registro
 router.post(
   "/auth/register",
+
   body("handle").notEmpty().withMessage("El handle no puede ir vacío"),
   body("name").notEmpty().withMessage("El nombre no puede ir vacío"),
   body("email").isEmail().withMessage("E-mail no válido"),
   body("password")
     .isLength({ min: 8 })
     .withMessage("El password es muy corto, mínimo 8 caracteres"),
+  handleInputErrors,
   createAccount
 );
+
 router.post(
   "/auth/login",
-  login,
+
   body("email").isEmail().withMessage("E-mail no válido"),
-  body("password")
-    .notEmpty()
-    .withMessage("El password es obligatorio")
+  body("password").notEmpty().withMessage("El password es obligatorio"),
+  handleInputErrors,
+  login
 );
 export default router;
